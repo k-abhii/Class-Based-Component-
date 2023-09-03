64f34376b5ce076b24b0d7e63ad133a1413b616b
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2023-09-03 10:54:52 +0530

    Started React Journey With class Based Components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import EmployeeClassComponent from "./EmployeeC";
+
+function App() {
+  return (
+    <div>
+      <EmployeeClassComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/EmployeeC.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class EmployeeClassComponent extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Employee Class Component</h1>
+      </div>
+    );
+  }
+}
+export default EmployeeClassComponent;
